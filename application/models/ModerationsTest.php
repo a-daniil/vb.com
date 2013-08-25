8b331ad8e2072fe18571bfabd39fe448ba263088
diman <diman@diman-HP-Mini-110-4100.(none)>
2013-08-25 20:49:16 +0400

    fix

---
+++ application/models/ModerationsTest.php
@@ -0,0 +1,18 @@
+<?php
+
+class Model_ModerationsTest extends Zend_Db_Table_Abstract {
+
+	protected $_name = 'moderations';
+
+	public function getByAnkId( $ank_id )
+	{
+		$select = $this->select();
+		$select->where('owner_id = ?', $ank_id);
+		
+		$row = $this->fetchAll($select);
+		if ( $row ) {
+			return $row->toArray();
+		}
+		return array();
+	}
+}
---
+++ application/models/UsersLog.php
@@ -0,0 +1,45 @@
+<?php 
+
+class Model_UsersLog extends Zend_Db_Table_Abstract {
+
+	const SUCCESS = 'success';
+	const FAILED  = 'failed';
+
+	protected $_name = 'users_log';
+
+	public function createSuccess( $username ) {
+		$this->insert(array(
+			"username" => $username,
+			"status"   => self::SUCCESS, 
+			"date"     => date('Y-m-d H:i:s')
+		));
+	}
+
+	public function createFailed( $username ) {
+		$this->insert(array(
+			"username" => $username,
+			"status"   => self::FAILED,
+			"date"     => date('Y-m-d H:i:s')
+		));
+	}
+
+	public function hasBanned( $username ) {
+		$sql = "SELECT COUNT(*) as count FROM
+				(SELECT * FROM users_log
+					 WHERE
+					 username = '" . $username ."'
+					 ORDER BY date DESC
+					 LIMIT 5) AS log2
+				WHERE
+					log2.status = 'failed'
+					AND log2.date > DATE_SUB(NOW(), INTERVAL 20 MINUTE)";
+		$statement = $this->getAdapter()->query($sql);
+		$rows = $statement->fetch();
+
+		if ( $rows['count'] == 5 ) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
