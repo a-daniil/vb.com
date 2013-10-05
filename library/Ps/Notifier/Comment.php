35704c83cbd9e48adcd509cee0aa502d81c58b8e
diman <diman@diman-HP-Mini-110-4100.(none)>
2013-10-05 20:53:17 +0400

    fix

--- library/Ps/Notifier/Comment.php
+++ library/Ps/Notifier/Comment.php
@@ -2,43 +2,48 @@
 
 class Ps_Notifier_Comment_Exception extends Zend_Exception{}
 
-class Ps_Notifier_Comment extends Ps_Notifier_Sender {	
-	
+class Ps_Notifier_Comment extends Ps_Notifier_Sender {
+
 	const MESSAGE = "У вашей анкеты ( %s ) новый комментарий.<br/>";
-	
-	public function send( $uid, $params = null ) 
-	{		
-		$link = $this->getAnketLink($params);		
-		$mail = $this->getUserMail($uid);		
+
+	public function send( $uid, $params = null )
+	{
+		$link = $this->getAnketLink($params);
+		$mail = $this->getUserMail($uid);
 		if ( !$mail ) {
 			throw new Ps_Notifier_Comment_Exception('User '. $uid . ' don\'t have email in DB');
 		}
-		
+
 		$content = Zend_Registry::get('content');
+		$config = Zend_Registry::get('config');
 		$header ="Content-type: text/html; charset=utf-8 \r\n";	
 		$header.="From: ".$content->mail->from->new_comment." \r\n";
 		
 		$text = sprintf(self::MESSAGE, $link);
 		$text.= "<p>Письмо отправлено автоматически отвечать на него не нужно.<br/>";
-		$text.= "С уважением, робот <a href='http://". $this->config->domen ."'>vb.arcada-team.ru</a>";
+		$text.= "С уважением, робот <a href='http://". $config->domen ."'>".$config->domen."</a>";
 		$subj = $content->mail->subj->new_comment;
-		
+
 		$result = mail($mail, $subj, $text, $header);
 		if ( !$result ) {
 			throw new Ps_Notifier_Comment_Exception('Can\'t send comment email');
 		}
-	}	
-	
+	}
+
 	private function getAnketLink( $params )
 	{
 		$comment = new Model_CommentsTest();
 		$owner_id = $comment->getOwnerId($params);
-		
+
 		$ankets = new Model_AnketsTest();
 		$ankets = $ankets->getById($owner_id);
-		
-		$link = "http://www.putanastars.com/anketa/" . $ankets['name_eng'] . "-" . $ankets['id'];
-		
+
+		/* get domen name */
+		$config = Zend_Registry::get('config');
+		$domen = $config->domen;
+
+		$link = "http://{$domen}/anketa/" . $ankets['name_eng'] . "-" . $ankets['id'];
+
 		return "<a href='". $link . "'>" . $link . "</a>";
 	}
 }
--- library/Ps/Notifier/Moderation.php
+++ library/Ps/Notifier/Moderation.php
@@ -14,14 +14,14 @@ class Ps_Notifier_Moderation extends Ps_Notifier_Sender {
 		if ( !$mail ) {
 			throw new Ps_Notifier_Moderation_Exception('User ' . $uid . ' don\'t have email in DB');
 		}
-		
+
 		$content = Zend_Registry::get('content');
 		$header ="Content-type: text/html; charset=utf-8 \r\n";	
 		$header.="From: ".$content->mail->from->new_moderation." \r\n";
-		
+
 		$text = $params['body'];
 		$subj = $content->mail->subj->new_moderation ." ". $params['subject'];
-		
+
 		$result = mail($mail, $subj, $text, $header);
 		if ( !$result ) {
 			throw new Ps_Notifier_Moderation_Exception('Can\'t send moder email');
